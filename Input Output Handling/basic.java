a834dc81f30f04942c0aa2d844d7a259b8be4eef
Kabilankavi141 <121872971+Kabilankavi141@users.noreply.github.com>
2023-05-14 09:04:39 -0400

    Input Output Concept Added

---
+++ Input Output Handling/basic.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class basic {
+    public static void main(String args[]){
+        Scanner scanf=new Scanner(System.in);
+        String name;
+        System.out.println("Enterr your name :");
+        name=scanf.nextLine();
+        System.out.println("Welcome "+name);
+    }
+}
